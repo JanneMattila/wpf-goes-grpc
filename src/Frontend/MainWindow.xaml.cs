1e03f8a3e5ff61d3a9dfdbba549757a6817469e8
Janne Mattila <janne.mattila@hotmail.fi>
2019-12-03 22:22:22 +0200

    Enable invalid certificate in demo scenario

--- src/Frontend/MainWindow.xaml.cs
+++ src/Frontend/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Grpc.Net.Client;
 using System.Windows;
 using Shared;
+using System.Net.Http;
 
 namespace Frontend
 {
@@ -16,7 +17,22 @@ namespace Frontend
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            var channel = GrpcChannel.ForAddress(BackendAddress.Text);
+#if DEBUG
+            // From https://docs.microsoft.com/en-us/aspnet/core/grpc/troubleshoot?view=aspnetcore-3.1
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            };
+            var httpClient = new HttpClient(handler);
+#else
+            var httpClient = new HttpClient();
+#endif
+
+            var channel = GrpcChannel.ForAddress(BackendAddress.Text,
+                new GrpcChannelOptions 
+                { 
+                    HttpClient = httpClient 
+                });
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
                 new HelloRequest

5b3d040ab029a7bfca7c89560c419aa98bd979bc
Janne Mattila <janne.mattila@hotmail.fi>
2019-12-03 20:42:40 +0200

    Initial structure

---
+++ src/Frontend/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Net.Client;
+using System.Windows;
+using Shared;
+
+namespace Frontend
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var channel = GrpcChannel.ForAddress(BackendAddress.Text);
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest
+                {
+                    Name = "GreeterClient"
+                });
+
+            SayHelloResponse.Text = reply.Message;
+        }
+    }
+}

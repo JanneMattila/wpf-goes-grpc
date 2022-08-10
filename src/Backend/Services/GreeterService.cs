7d05cc3e14798bcc50bd936de3a7df1d5b71e75d
Janne Mattila <janne.mattila@hotmail.fi>
2022-08-10 13:58:24 +0300

    Update to .NET 6

--- src/Backend/Services/GreeterService.cs
+++ src/Backend/Services/GreeterService.cs
@@ -1,25 +1,25 @@
-using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 using Shared;
+using System;
+using System.Threading.Tasks;
 
-namespace Backend
+namespace Backend;
+
+public class GreeterService : Greeter.GreeterBase
 {
-    public class GreeterService : Greeter.GreeterBase
+    private readonly ILogger<GreeterService> _logger;
+
+    public GreeterService(ILogger<GreeterService> logger)
     {
-        private readonly ILogger<GreeterService> _logger;
+        _logger = logger;
+    }
 
-        public GreeterService(ILogger<GreeterService> logger)
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
         {
-            _logger = logger;
-        }
-
-        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-        {
-            return Task.FromResult(new HelloReply
-            {
-                Message = "Hello " + request.Name
-            });
-        }
+            Message = $"Hello {request.Name} from {Environment.MachineName}!"
+        });
     }
 }
